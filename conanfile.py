b9e327213e5dea03764ac444da64434c831d3e1c
ske1337 <ske1337@gmail.com>
2024-06-08 15:02:22 +0200

    Fix the conanfile.py:

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,5 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.gnu import Autotools, AutotoolsToolchain
-from conan.tools.apple import is_apple_os
 
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -9,12 +7,6 @@ class CompressorRecipe(ConanFile):
 
     def requirements(self):
         self.requires("openssl/3.2.2")
-
-    def generate(self):
-        tc = AutotoolsToolchain(self)
-        if is_apple_os(self):
-            tc.extra_cflags.append("-arch x86_64 -arch arm64")
-        tc.generate()
 
     def layout(self):
         cmake_layout(self)
