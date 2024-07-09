efaba301d392c049d4790927c2e0da31afa41990
ske1337 <ske1337@gmail.com>
2024-07-09 07:01:27 +0200

    Wip

--- tests/core/file/BinaryModificationTest.cpp
+++ tests/core/file/BinaryModificationTest.cpp
@@ -15,8 +15,8 @@ TEST(BinaryModificationTest, CanModifyUsingDiff)
 {
     // x86 size: 246818 bytes... shit
     //
-    const auto source = x86exe->binary()->part(200, 300).bytes();
-    const auto target = x86_64dll->binary()->part(200, 300).bytes();
+    const auto source = x86exe->binary()->part(0, 30).bytes();
+    const auto target = x86_64dll->binary()->part(50, 30).bytes();
 
     const auto sourceHash = hasher->sha256FromBytes(source);
     const auto targetHash = hasher->sha256FromBytes(target);
@@ -35,16 +35,18 @@ TEST(BinaryModificationTest, CanModifyUsingDiff)
     const auto modifiedDiff = CDiffExtractor::extract(modified, target);
     const auto modifiedHash = hasher->sha256FromBytes(modified);
 
-    const auto modificationTwo = CBinaryModification {
-        CUuid { "BINARY_TRANSLATION" },
-        BinaryModificationType::APPEND_CODE,
-        modifiedDiff,
-        { }
-    };
+    if (modifiedDiff.size() > 0) {
+        const auto modificationTwo = CBinaryModification {
+            CUuid { "BINARY_TRANSLATION" },
+            BinaryModificationType::APPEND_CODE,
+            modifiedDiff,
+            { }
+        };
 
-    const auto finalModified = modificationTwo.apply(modified);
-    const auto finalModifiedDiff = CDiffExtractor::extract(finalModified, target);
-    const auto finalModifiedHash = hasher->sha256FromBytes(finalModified);
+        const auto finalModified = modificationTwo.apply(modified);
+        const auto finalModifiedDiff = CDiffExtractor::extract(finalModified, target);
+        const auto finalModifiedHash = hasher->sha256FromBytes(finalModified);
+    }
 
     ASSERT_EQ(modified.size(), target.size());
     ASSERT_STREQ(modifiedHash.c_str(), targetHash.c_str());
